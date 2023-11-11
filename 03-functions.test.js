2b2a33931aab08b9b6a519274073fbd78e114a62
FelixRG <correofrg@gmail.com>
2023-11-11 21:37:07 +0100

    Finalizado

--- 03-functions.test.js
+++ 03-functions.test.js
@@ -1,5 +1,6 @@
 import {describe, it} from 'node:test';
 import assert from 'node:assert/strict';
+import { clear } from 'node:console';
 
 
 // SEGUNDA PARTE: Las funciones en JavaScript. Lee bien los tests para averiguar qué se pide en cada momento, y crea una función que haga pasar el test.
@@ -7,19 +8,24 @@ import assert from 'node:assert/strict';
 describe("Funciones en JavaScript", () => {
     it("Las funciones pueden tener 1 parámetro", () => {
         let name = "Rosa";
-
-        // Escribe aquí tu funcion "saludar"
-
+        
+        function saludar(name){
+            return "Hola, " + name;
+        }
+    
 
         assert.strictEqual(saludar(name),"Hola, Rosa");
     })
+})
 
 
     it("Las funciones pueden tener 2 parámetros", () => {
         let num1 = 17;
         let num2 = 10;
 
-        // Escribe aquí tu función "suma"
+        function suma(a,b){
+        return a + b;
+        }
 
 
         assert.strictEqual(suma(num1, num2),27);
@@ -33,9 +39,11 @@ describe("Funciones en JavaScript", () => {
         let weather = "soleado";
         let rainProbability = "10%";
 
-        // Escribe aquí tu función "weatherReport" (fíjate en la aserción para saber qué debe retornar esta función).
+        function weatherReport(userName, name, city, weather, rainProbability){
+            return `Hola ${name} (${userName}), hoy en ${city} el tiempo es ${weather} y las probabilidades de lluvia son del ${rainProbability}.`;
+        }
 
-        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability)).toEqual("Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
+        assert.strictEqual(weatherReport(userName, name, city, weather, rainProbability),"Hola Ada Martín (ada89), hoy en Las Vegas el tiempo es soleado y las probabilidades de lluvia son del 10%.");
     })
 
 
@@ -43,17 +51,18 @@ describe("Funciones en JavaScript", () => {
     // BONUS: Crea un test que te permita testear la función que verás a continuación. Para ello, también te hemos proporcionado las variables que necesitarás (y alguna más que no necesitarás, para confundir :P).
     // Recuerda quitar la puntuación de comentario (/* ... */) para que este código a continuación sea legible, e inclúyelo dentro de tu test.
 
-
-     /*    let userName = "Ariana28";
+    it("Las funciones pueden tener múltiples parámetros", () => {
+        let userName = "Ariana28";
         let userAge = 38;
         let userEmail = "ariana_28@gmail.com";
         let userLocation = "Barcelona"
         let userIsRegistered = true;
         let password = "p4s$w0rDs3guR0";
 
-        function userAuth(userName, userEmail, password) {
-            return "Welcome ${userName}, your email is ${userEmail} and your password is ${password}."
-        } */
-
-
+        function userAuth(userName, userEmail, password){
+            return `Welcome ${userName}, your email is ${userEmail} and your password is ${password}.`;
+        }
+        assert.strictEqual(userAuth(userName, userEmail, password),"Welcome Ariana28, your email is ariana_28@gmail.com and your password is p4s$w0rDs3guR0.");
+    
     })
+    
